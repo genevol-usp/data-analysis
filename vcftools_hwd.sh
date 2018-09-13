8674ca26c56f1d7afd9a274b03d7d4a8cde86f5e
VitorAguiar <vitor.aguiar@me.com>
2018-09-12 21:06:27 -0300

    add sample ids file and option to filter pop in vcftools_hwd.sf

--- vcftools_hwd.sh
+++ vcftools_hwd.sh
@@ -7,8 +7,15 @@ file=ALL.chr21.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz
 
 # montar lista de amostras YRI para analise.
 grep YRI  samples_all > yri_samples
+#awk '$2 ~ /YRI/ {print $1}' ./data/sample_info.tsv > yri_samples
+
 
 # geracao de arquivo com hw e outro com frquencias
-vcftools --gzvcf $datadir/$file --freq --max-alleles 2 --chr 21  --out chr21
-vcftools --gzvcf $datadir/$file --chr 21 --max-alleles 2 --hardy  --out chr21
+vcftools --gzvcf $datadir/$file --freq --max-alleles 2 --chr 21 --out chr21
+#vcftools --gzvcf $datadir/$file --keep yri_samples --freq --max-alleles 2 --chr 21 --out chr21
+
+vcftools --gzvcf $datadir/$file --chr 21 --max-alleles 2 --hardy --out chr21
+#vcftools --gzvcf $datadir/$file --keep yri_samples --chr 21 --max-alleles 2 --hardy --out chr21
+
 vcftools --gzvcf $datadir/$file --chr 21 --keep-only-indels --max-alleles 2 --hardy --out chr21_indels
+#vcftools --gzvcf $datadir/$file --keep yri_samples --chr 21 --keep-only-indels --max-alleles 2 --hardy --out chr21_indels
